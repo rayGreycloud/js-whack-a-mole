27c2d080ea5ac7bd886112043ca02f74ad790cd3
rayGreycloud <rayGreycloud@gmail.com>
2016-12-22 07:27:49 -0800

    Add popUp function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
 
 function randomTime(min, max) {
   return Math.random() * (max - min) + min;
@@ -15,4 +16,14 @@ function randomHole(holes) {
   }
   lastHole = hole;
   return hole;
+}
+
+function popUp() {
+  const time = randomTime(400, 1100);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) popUp();
+  }, time);
 }
